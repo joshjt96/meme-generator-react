b1f1cb5cfa36a92d27bac169359b1a90dddb3a56
Josh Talley <jtdev84@gmail.com>
2022-11-03 11:41:26 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header"
+
+/**
+ * Challenge: Build the Header component
+ */
+function App() {
+    return (
+        <Header />
+    )
+    
+}
+
+export default App
---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header(props) {
+    return(
+        <div>
+            <nav className="header">
+                <h1 className="header-h">Meme Generator</h1>
+                <h2 className="header-h">React Course - Project 3</h2>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
